c21538b4a762a7090f3dc5b08fa9232a14766e14
xiaomi <xiaomi@robotest.dev>
2026-07-14 21:41:51 +0800

    :sparkles: API 错误日志框架集成
    - 实现 ApiErrorLogFrameworkService 接口
    - 预留错误日志记录扩展点

---
+++ server/src/main/java/io/github/xiaomisum/robotest/framework/ApiErrorLogFrameworkServiceImpl.java
@@ -0,0 +1,14 @@
+package io.github.xiaomisum.robotest.framework;
+
+import org.springframework.stereotype.Component;
+import xyz.migoo.framework.apilog.core.ApiErrorLog;
+import xyz.migoo.framework.apilog.core.ApiErrorLogFrameworkService;
+
+@Component
+public class ApiErrorLogFrameworkServiceImpl implements ApiErrorLogFrameworkService {
+
+    @Override
+    public void createApiErrorLog(ApiErrorLog apiErrorLog) {
+
+    }
+}
