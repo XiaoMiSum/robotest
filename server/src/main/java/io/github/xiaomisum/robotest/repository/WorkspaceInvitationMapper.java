973d95ba8dbd7214d83668ff4120cf4e19edadb5
xiaomi <mi_xiao@qq.com>
2026-07-19 20:12:41 +0800

    ✨ feat(repository): 添加空间管理模块 Mapper 接口
    - 新增 WorkspaceInvitationMapper 邀请链接数据访问
    - 新增 ProjectMapper 项目数据访问

---
+++ server/src/main/java/io/github/xiaomisum/robotest/repository/WorkspaceInvitationMapper.java
@@ -0,0 +1,9 @@
+package io.github.xiaomisum.robotest.repository;
+
+import io.github.xiaomisum.robotest.model.entity.WorkspaceInvitation;
+import org.apache.ibatis.annotations.Mapper;
+import xyz.migoo.framework.mybatis.core.BaseMapperX;
+
+@Mapper
+public interface WorkspaceInvitationMapper extends BaseMapperX<WorkspaceInvitation> {
+}
