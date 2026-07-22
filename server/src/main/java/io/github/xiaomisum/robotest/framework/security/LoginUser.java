eb7c51ec4fa35914544fda9e125a4e9f1b0e5fae
xiaomi <mi_xiao@qq.com>
2026-07-22 10:36:41 +0800

    ♻️ refactor(server): LoginUser 泛型从 String 改为 UUID + 移除 roleNames/permissionCodes，改用父类 setAuthorities

--- server/src/main/java/io/github/xiaomisum/robotest/framework/security/LoginUser.java
+++ server/src/main/java/io/github/xiaomisum/robotest/framework/security/LoginUser.java
@@ -3,26 +3,18 @@ package io.github.xiaomisum.robotest.framework.security;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import xyz.migoo.framework.security.core.AuthUserDetails;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Stream;
 
 @Getter
 @Setter
-public class LoginUser extends AuthUserDetails<LoginUser, String> {
+public class LoginUser extends AuthUserDetails<LoginUser, UUID> {
 
     private String email;
-
-    private Set<String> roleNames;
-
-    private Set<String> permissionCodes;
 
     /**
      * 工作空间角色追加的权限（由 WorkspaceRoleInterceptor 注入），与系统权限合并后返回。
@@ -32,19 +24,13 @@ public class LoginUser extends AuthUserDetails<LoginUser, String> {
 
     @Override
     @JsonIgnore
+    @NonNull
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        Stream<String> roleStream = roleNames != null
-                ? roleNames.stream().map(r -> "ROLE_" + r)
-                : Stream.empty();
-        Stream<String> permStream = permissionCodes != null
-                ? permissionCodes.stream()
-                : Stream.empty();
-        Stream<GrantedAuthority> wsStream = workspaceAuthorities != null
+        Stream<? extends GrantedAuthority> baseStream =  super.getAuthorities().stream();
+        Stream<? extends GrantedAuthority> wsStream = workspaceAuthorities != null
                 ? workspaceAuthorities.stream()
                 : Stream.empty();
-        return Stream.concat(Stream.concat(roleStream, permStream).map(SimpleGrantedAuthority::new), wsStream)
-                .distinct()
-                .toList();
+        return Stream.concat(baseStream, wsStream).distinct().toList();
     }
 
     /**
@@ -54,30 +40,5 @@ public class LoginUser extends AuthUserDetails<LoginUser, String> {
         if (authorities != null) {
             this.workspaceAuthorities.addAll(authorities);
         }
-    }
-
-    @Override
-    public LoginUser setId(String id) {
-        return super.setId(id);
-    }
-
-    @Override
-    public LoginUser setUsername(String username) {
-        return super.setUsername(username);
-    }
-
-    @Override
-    public LoginUser setPassword(String password) {
-        return super.setPassword(password);
-    }
-
-    @Override
-    public LoginUser setName(String name) {
-        return super.setName(name);
-    }
-
-    @Override
-    public LoginUser setEnabled(boolean enabled) {
-        return super.setEnabled(enabled);
     }
 }
