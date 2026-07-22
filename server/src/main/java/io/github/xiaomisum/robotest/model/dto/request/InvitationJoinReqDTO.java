9bbfedcb4a38c16cfe5f2196fd3157871f18097a
unknown <L0544@nistone.local>
2026-07-22 10:37:01 +0800

    ♻️ refactor(server): 使用 migoo 框架 @Password/@Email 注解替代手动验证

--- server/src/main/java/io/github/xiaomisum/robotest/model/dto/request/InvitationJoinReqDTO.java
+++ server/src/main/java/io/github/xiaomisum/robotest/model/dto/request/InvitationJoinReqDTO.java
@@ -1,8 +1,9 @@
 package io.github.xiaomisum.robotest.model.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import xyz.migoo.framework.common.validation.Email;
+import xyz.migoo.framework.common.validation.Password;
 
 @Data
 public class InvitationJoinReqDTO {
@@ -15,5 +16,6 @@ public class InvitationJoinReqDTO {
     private String email;
 
     @NotBlank(message = "密码不能为空")
+    @Password(message = "密码强度不够，需包含大小写字母、数字和特殊字符")
     private String password;
 }
--- server/src/main/java/io/github/xiaomisum/robotest/model/dto/request/UserPasswordResetReqDTO.java
+++ server/src/main/java/io/github/xiaomisum/robotest/model/dto/request/UserPasswordResetReqDTO.java
@@ -3,11 +3,13 @@ package io.github.xiaomisum.robotest.model.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import xyz.migoo.framework.common.validation.Password;
 
 @Data
 public class UserPasswordResetReqDTO {
 
     @NotBlank(message = "新密码不能为空")
     @Size(min = 8, max = 64, message = "密码长度为8-64个字符")
+    @Password(message = "密码强度不够，需包含大小写字母、数字和特殊字符")
     private String newPassword;
 }
